8f17e3b9e4b1820690c3e99832edecc021dc6870
ve3 <kawewong@gmail.com>
2020-11-19 17:31:08 +0700

    Add new JS file that especially work on admin public pages.

---
+++ assets/js/Controllers/Admin/UI/commonAdminPublic/RdbaCommonAdminPublic.js
@@ -0,0 +1,62 @@
+/**
+ * JS file for common admin where expose to public such as login, register, forgot password, etc.
+ */
+
+
+/**
+ * JS Admin class that use with the page expose to public.
+ * 
+ * @since 1.1.0
+ */
+class RdbaCommonAdminPublic {
+
+
+    /**
+     * Listen on change language and set new language.
+     * 
+     * Should contain these elements to work.
+     * <pre>
+     * &lt;select id=&quot;rundizbones-languages-selectbox&quot; class=&quot;rundizbones-languages-selectbox&quot; name=&quot;rundizbones-languages&quot;&gt;
+     * &lt;option value=&quot;[LANGUAGE ID]&quot;&gt;[LANGUAGE NAME]&lt;/option&gt;
+     * &lt;/select&gt;
+     * &lt;input id=&quot;rdbaCurrentUrl&quot; type=&quot;hidden&quot; name=&quot;currentUrl&quot; value=&quot;[YOUR URL]&quot;&gt;
+     * &lt;input id=&quot;rdbaSetLanguage_url&quot; type=&quot;hidden&quot; name=&quot;setLanguage_url&quot; value=&quot;[URL TO SET LANGUAGE]&quot;&gt;
+     * &lt;input id=&quot;rdbaSetLanguage_method&quot; type=&quot;hidden&quot; name=&quot;setLanguage_method&quot; value=&quot;[SET LANGUAGE METHOD]&quot;&gt;
+     * </pre>
+     * 
+     * @return {undefined}
+     */
+    static listenOnChangeLanguage() {
+        let languageSelectbox = document.getElementById('rundizbones-languages-selectbox');
+
+        if (languageSelectbox) {
+            languageSelectbox.addEventListener('change', function(event) {
+                event.preventDefault();
+
+                let setLanguageUrl = document.getElementById('rdbaSetLanguage_url');
+                let setLanguageMethod = document.getElementById('rdbaSetLanguage_method');
+                let currentUrl = document.getElementById('rdbaCurrentUrl');
+
+                RdbaCommon.XHR({
+                    'url': (setLanguageUrl ? setLanguageUrl.value : ''),
+                    'method': (setLanguageMethod ? setLanguageMethod.value : ''),
+                    data: 'currentUrl=' + (currentUrl ? currentUrl.value : '') + '&rundizbones-languages=' + languageSelectbox.value,
+                    dataType: 'json'
+                })
+                .catch(function(responseObject) {
+                    // XHR failed.
+                    console.error(responseObject);
+                })
+                .then(function(responseObject) {
+                    // XHR success.
+                    let response = responseObject.response;
+                    if (typeof(response.redirectUrl) !== 'undefined') {
+                        window.location.href = response.redirectUrl;
+                    }
+                });
+            });
+        }
+    }// listenOnChangeLanguage
+
+
+}// RdbaCommonAdminPublic
