e80ba7e31d3005be88842c146cda285138e0554b
ve3 <kawewong@gmail.com>
2021-02-09 17:19:42 +0700

    Update AssetsTest.php

--- Tests/Libraries/AssetsTest.php
+++ Tests/Libraries/AssetsTest.php
@@ -548,10 +548,9 @@ class AssetsTest extends \Rdb\Tests\BaseTestCase
 
         $item['file'] = $publicModuleUrl . '/assets/' . $this->testAssetFolderName . '/jquery.js?v=view';
         $item['version'] = '3.x.x';
-        $this->assertEquals(
-            $item['file'] . '&v_ee7bb2d456bfb61730220741f8b33eca63cfbd3c7652d9ff1a009592cc1ad7715abf153e89ef8e41a0bee557be5633bdb4464103a81c56e0d8f53abe4c54980e=3.x.x', 
-            $this->Assets->generateAssetUrlWithVersion($item)
-        );// already has v=xxx so it must not overwrite.
+        $result = $this->Assets->generateAssetUrlWithVersion($item);
+        $this->assertStringContainsString($item['file'], $result);// already has v=xxx so it must not overwrite.
+        $this->assertStringContainsString('=3.x.x', $result);// already has v=xxx so it must not overwrite.
 
         // tests with full URL or CDN. ----------------------------------
         $item['file'] = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js';
