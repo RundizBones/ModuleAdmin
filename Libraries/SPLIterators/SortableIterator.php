c64bd561b63ba6df79386d2d34a182021db8f0e0
ve3 <kawewong@gmail.com>
2022-02-19 18:58:51 +0700

    Fix return type not compatible error in PHP 8.1.

--- Libraries/SPLIterators/SortableIterator.php
+++ Libraries/SPLIterators/SortableIterator.php
@@ -86,7 +86,7 @@ class SortableIterator implements \IteratorAggregate
     /**
      * @return \Traversable
      */
-    public function getIterator()
+    public function getIterator(): \Traversable
     {
         if (1 === $this->sort) {
             return $this->iterator;
