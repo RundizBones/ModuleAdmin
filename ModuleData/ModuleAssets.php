5ca1a2a37007dfadeaacbcaacd064d315dd95636
ve3 <kawewong@gmail.com>
2022-08-03 23:06:37 +0700

    Update Node packages version.

--- ModuleData/ModuleAssets.php
+++ ModuleData/ModuleAssets.php
@@ -50,7 +50,7 @@ class ModuleAssets
                 [
                     'handle' => 'rdta',
                     'file' => $publicModuleUrl . '/assets/css/rdta/rdta-bundled.min.css',
-                    'version' => '2.2.2',
+                    'version' => '2.2.3',
                 ],
                 // datatables----------------.
                 [
@@ -93,7 +93,7 @@ class ModuleAssets
                 [
                     'handle' => 'rdta',
                     'file' => $publicModuleUrl . '/assets/js/rdta/rdta-bundled.min.js',
-                    'version' => '2.2.2',
+                    'version' => '2.2.3',
                 ],
                 [
                     'handle' => 'handlebars',
