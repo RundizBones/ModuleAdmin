6504451d1972301397ebd09d0ec1a87977afde3e
ve3 <kawewong@gmail.com>
2023-05-31 12:07:29 +0700

    Move call to `isEditingHigherRole` from trait instead.

--- Controllers/Admin/Users/PreviousEmailsController.php
+++ Controllers/Admin/Users/PreviousEmailsController.php
@@ -22,6 +22,9 @@ class PreviousEmailsController extends \Rdb\Modules\RdbAdmin\Controllers\Admin\A
     use Traits\UsersTrait;
 
 
+    use Traits\UsersEditingTrait;
+
+
     public function indexAction($user_id)
     {
         // processing part ----------------------------------------------------------------------------------------------------
@@ -112,71 +115,6 @@ class PreviousEmailsController extends \Rdb\Modules\RdbAdmin\Controllers\Admin\A
             return $this->Views->render('common/Admin/mainLayout_v', $output);
         }
     }// indexAction
-
-
-    /**
-     * Check if target user has higher priority role than a user who is editing them.
-     * 
-     * @param string|int $user_id The target user ID.
-     * @return bool Return `false` if not editing higher priority role, return `true` if yes.
-     */
-    protected function isEditingHigherRole($user_id): bool
-    {
-        if (!is_numeric($user_id)) {
-            // if target user_id is not number.
-            // return true to prevent user from editing.
-            return true;
-        }
-
-        if (
-            isset($this->userSessionCookieData) && 
-            is_array($this->userSessionCookieData) && 
-            array_key_exists('user_id', $this->userSessionCookieData)
-        ) {
-            $UsersRolesDb = new \Rdb\Modules\RdbAdmin\Models\UsersRolesDb($this->Container);
-            $options = [];
-            $options['where']['user_id'] = $this->userSessionCookieData['user_id'];
-            $options['limit'] = 1;
-            $options['sortOrders'] = [['sort' => 'userrole_priority', 'order' => 'ASC']];
-            $myRoles = $UsersRolesDb->listItems($options);
-            unset($options);
-
-            if (isset($myRoles['items'])) {
-                $myRoles = array_shift($myRoles['items']);
-
-                $options = [];
-                $options['where']['user_id'] = $user_id;
-                $targetRoles = $UsersRolesDb->listItems($options);
-                unset($options);
-
-                if (isset($targetRoles['items']) && is_array($targetRoles['items']) && isset($targetRoles['total'])) {
-                    $i = 0;
-                    foreach ($targetRoles['items'] as $row) {
-                        if ($row->userrole_priority < $myRoles->userrole_priority) {
-                            // if target user's role is higher priority than user who is editing.
-                            // return true to prevent user from editing.
-                            unset($myRoles, $row, $targetRoles, $UsersRolesDb);
-                            return true;
-                        }
-                        $i++;
-                    }// endforeach;
-                    unset($row);
-
-                    if (isset($i) && $i == $targetRoles['total']) {
-                        unset($myRoles, $targetRoles, $UsersRolesDb);
-                        return false;
-                    }
-                }
-
-                unset($targetRoles);
-            }
-
-            unset($myRoles, $UsersRolesDb);
-        }
-
-        // return true to prevent user from editing.
-        return true;
-    }// isEditingHigherRole
 
 
 }
