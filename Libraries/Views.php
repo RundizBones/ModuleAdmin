587e7b97fdac35b3d6036a5e72c96a07db20679f
ve3 <kawewong@gmail.com>
2025-04-16 10:06:08 +0700

    **Libraries/Views.php:** Add `locateViews()` for auto detect caller module.

--- Libraries/Views.php
+++ Libraries/Views.php
@@ -29,6 +29,32 @@ class Views extends \Rdb\System\Views
 
     /**
      * {@inheritDoc}
+     */
+    public function locateViews(string $viewsFile, ?string $currentModule = null): string
+    {
+        if (empty($currentModule)) {
+            $debugBacktrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
+            if (
+                isset($debugBacktrace[3]['class']) && 
+                is_scalar($debugBacktrace[3]['class']) &&
+                stripos($debugBacktrace[3]['class'], 'Rdb\\Modules\\') !== false
+            ) {
+                // if found modules in trace.
+                $expClass = explode('\\', $debugBacktrace[3]['class']);
+                if (isset($expClass[2])) {
+                    // if found the module name.
+                    $currentModule = $expClass[2];
+                }
+            }// endif;
+            unset($debugBacktrace, $expClass);
+        }
+
+        return parent::locateViews($viewsFile, $currentModule);
+    }// locateViews
+
+
+    /**
+     * {@inheritDoc}
      * 
      * Also set required data if not exists.
      */
